3eb5cc277ebda457f5ba62de0b52a84009c63b9f
Céline Gourc <celine.gourc1@laposte.net>
2023-04-27 16:10:48 +0200

    changement index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import reportWebVitals from './reportWebVitals'
+//import reportWebVitals from './reportWebVitals'
 import { RouterProvider } from "react-router-dom"
 import { router } from "./RoutesPath.jsx"
 import Footer from './components/Footer/Footer.jsx'
-
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -21,5 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+//reportWebVitals()
+
 
